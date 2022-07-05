72a47b547e49b03db1dc665b1c53c511b43227fa
EpiclyRaspberry <raspberryepicly@gmail.com>
2022-07-06 02:19:48 +0800

    rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-   name="asyncat",
+   name="popcat",
    version="0.0.1",
    author="EpiclyRaspberry",
    author_email="raspberryepicly@gmail.com",
